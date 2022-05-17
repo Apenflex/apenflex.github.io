2bf86d55bd654ceb8672aa4ab18674afd39a51d9
Sviatoslav <98613056+Apenflex@users.noreply.github.com>
2022-05-17 22:01:55 +0300

    Add files via upload

--- site21/mailer/smart.php
+++ site21/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'blackdbf@gmail.com';                 // Наш логин
-$mail->Password = 'mppgqpvswukktdjm';                           // Наш пароль от ящика
+$mail->Password = 'UFFNGT3T';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
