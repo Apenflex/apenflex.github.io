222425d46ec2e0004cb8658157c6208eb322655e
Svyatoslav <98613056+Apenflex@users.noreply.github.com>
2022-08-12 12:06:16 +0300

    Add files via upload

---
+++ Car Rental Site/js/form.js
@@ -0,0 +1,47 @@
+(function () {
+    const modalOverlay = document.querySelector(".overlay_block");
+    const submitModal = document.querySelector(".button_offer");
+    const modalCloseButtons = document.querySelectorAll(".modal__close");
+    const modalMini = document.querySelector(".modal_mini");
+
+    modalCloseButtons.forEach(function (item) {
+        item.addEventListener("click", function (e) {
+            if (modalOverlay.classList.contains("overlay_active")) {
+            modalOverlay.classList.remove("overlay_active");
+            modalMini.classList.remove("modal_mini_active");
+            }
+        });
+    });
+
+
+function handleSubmit(event) {
+    if (!form.checkValidity()) return;
+  event.preventDefault();
+  const modalMini = document.querySelector(".modal_mini");
+  fetch("https://formspree.io/f/xjvzvwpq", {
+    method: "POST",
+    body: new FormData(form),
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+        modalOverlay.classList.add("overlay_active");
+        modalMini.classList.add("modal_mini_active");
+        form.reset();
+    } else {
+        modalOverlay.classList.add("overlay_active");
+        modalMini.classList.add("modal_mini_active");
+        document.querySelector('.modal_error_oops').innerHTML = "Oops!";
+        document.querySelector('.modal_error').innerHTML = "There was a problem submitting your form";
+    }
+  }).catch(error => {
+    modalOverlay.classList.add("overlay_active");
+    modalMini.classList.add("modal_mini_active");
+    document.querySelector('.modal_error_oops').innerHTML = "Oops!";
+    document.querySelector('.modal_error').innerHTML = "There was a problem submitting your form";
+  });
+}
+submitModal.addEventListener("click", handleSubmit);
+
+})();
